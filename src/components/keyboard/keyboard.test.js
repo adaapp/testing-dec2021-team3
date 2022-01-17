7eee9fa821d280342a646cf634e302528b921450
Bruno Silva <bruno.silva@ada.ac.uk>
2022-01-17 19:11:07 +0000

    Added Github Action for tests, improved App.js tests

---
+++ src/components/keyboard/keyboard.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from '@testing-library/react';
+import Keyboard from './keyboard';
+
+it('should...', () => {
+
+});
--- src/routes/App.test.js
+++ src/routes/App.test.js
@@ -1,18 +1,52 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-it('should display keyboard keys', () => {
+it('should display the name of the app ', () => {
   render(<App />);
-  let linkElement = screen.getByText(/1/i);
-  expect(linkElement).toBeInTheDocument();
-  linkElement = screen.getByText(/h/i);
-  expect(linkElement).toBeInTheDocument();
-  linkElement = screen.getByText(/3/i);
-  expect(linkElement).toBeInTheDocument();
+  
+  expect(screen.getByTestId('app-text')).toHaveTextContent('mélodie');
+  expect(screen.getByTestId('app-text')).toBeInstanceOf(HTMLSpanElement);
 });
 
-it('should display the app name', () => {
+it('should display the app logo at the top of the page', () => {
   render(<App />);
-  let linkElement = screen.getByText(/mélodie/i);
-  expect(linkElement).toBeInTheDocument();
+
+  expect(screen.getByAltText('Header Logo')).toBeInTheDocument;
+  expect(screen.getByAltText('Header Logo')).toBeInstanceOf(Image);
+
+  expect(screen.getByAltText('Header Logo')).toBeVisible();
+  expect(screen.getByAltText('Header Logo').id).toBe('logo');
+  expect(screen.getByAltText('Header Logo').src).toBe('http://localhost/svg/logo.svg');
 });
+
+it('should display every animated shape', () => {
+  render(<App />);
+
+  expect(screen.getByAltText('Visual Effect 1')).toBeVisible();
+  expect(screen.getByAltText('Visual Effect 1')).toBeInstanceOf(Image);
+
+  expect(screen.getByAltText('Visual Effect 2')).toBeVisible();
+  expect(screen.getByAltText('Visual Effect 2')).toBeInstanceOf(Image);
+
+  expect(screen.getByAltText('Visual Effect 3')).toBeVisible();
+  expect(screen.getByAltText('Visual Effect 3')).toBeInstanceOf(Image);
+});
+
+
+// it('should display keyboard keys', () => {
+//   render(<App />);
+
+//   let linkElement = screen.getByText(/1/i);
+//   expect(linkElement).toBeInTheDocument();
+//   linkElement = screen.getByText(/f/i);
+//   expect(linkElement).toBeInTheDocument();
+//   linkElement = screen.getByText(/3/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// it('should display the app name', () => {
+//   render(<App />);
+  
+//   let linkElement = screen.getByText(/mélodie/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
