de5225309224606ad1bdb4304d866eaa7d8576a3
elijaholan <eli.olanipekun@gmail.com>
2022-01-19 11:10:36 +0000

    Improved comments for the App.js file

--- src/routes/App.js
+++ src/routes/App.js
@@ -14,18 +14,45 @@ import keys from "../sounds/keys.mp3";
 import { createSprite, convertToNote } from '../helpers';
 
 function App() {
+  /* The state of a React app stores information about the current states
+  of components. The following three lines make use of a React 'Hook.'
+  Previously, you needed to write components as classes in order to
+  access React features like the state. Hooks get rid of the need 
+  for those classes! */
+
+  // Used to control whether the splash screen should be shown
   let [splash, setSplash] = useState(true);
+
+  // Stores the value of the note bubble underneath the keyboard
   let [currentKey, setKey] = useState('🎹');
+
+  // Stores the current volume for both the keyboard and metronome
   let [volume, setVolume] = useState('50');
+
+  /* A list containing all keyboard keys for the Piano. Howler.js, a
+     popular audio player package, is able to use a sprite, to store
+     divided parts of a single audio file. In order to create a
+     Sprite, we need to give each Piano key sample a value for 
+     reference. The values in the following list will be used
+     as references. */
 
   const ordered = ['$', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E',
     'f', 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'l', 'L', 'm', '!', '%', '(',
     '*', '@', '^', 'n', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 's', 'S', 't', 'T', 'u',
     'v', 'V', 'w', 'W', 'x', 'y', 'Y', 'z', 'Z'];
   
+  /* Uses the createSprite() helper function to create the object with a 
+  sprite and volume, in a format that Howler.js accepts. */
+
   const map = createSprite(ordered, 50);
+
+  /* use-sound is an example of a React 'Hook.' This particular hook
+  wraps the Howler.js library so that we can easily access it in React,
+  whilst interacting with components and the state. */
+
   const [play, data] = useSound(keys, map);
 
+  // The following return includes all of the HTML code for this page
   return (
     <div className='App'>
 
